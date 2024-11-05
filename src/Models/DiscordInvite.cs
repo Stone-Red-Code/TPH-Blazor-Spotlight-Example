0a33d7313b287212567b09de0d2a27f59c9fa6f4
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2024-11-05 23:50:16 +0100

    Add discord statistics and new year countdown

---
+++ src/Models/DiscordInvite.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TphBlazorSpotlightExample.Models;
+
+public class DiscordInvite
+{
+    [JsonPropertyName("type")]
+    public int Type { get; set; }
+
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("expires_at")]
+    public object ExpiresAt { get; set; }
+
+    [JsonPropertyName("flags")]
+    public int Flags { get; set; }
+
+    [JsonPropertyName("guild")]
+    public Guild Guild { get; set; }
+
+    [JsonPropertyName("guild_id")]
+    public string GuildId { get; set; }
+
+    [JsonPropertyName("approximate_member_count")]
+    public int ApproximateMemberCount { get; set; }
+
+    [JsonPropertyName("approximate_presence_count")]
+    public int ApproximatePresenceCount { get; set; }
+}
---
+++ src/Models/Guild.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TphBlazorSpotlightExample.Models;
+
+public class Guild
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("splash")]
+    public string Splash { get; set; }
+
+    [JsonPropertyName("banner")]
+    public string Banner { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("icon")]
+    public string Icon { get; set; }
+
+    [JsonPropertyName("features")]
+    public List<string> Features { get; set; }
+
+    [JsonPropertyName("verification_level")]
+    public int VerificationLevel { get; set; }
+
+    [JsonPropertyName("vanity_url_code")]
+    public string VanityUrlCode { get; set; }
+
+    [JsonPropertyName("nsfw_level")]
+    public int NsfwLevel { get; set; }
+
+    [JsonPropertyName("nsfw")]
+    public bool Nsfw { get; set; }
+
+    [JsonPropertyName("premium_subscription_count")]
+    public int PremiumSubscriptionCount { get; set; }
+}
