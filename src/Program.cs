d4538637f37bfe5f401f1d6d0a27c07fa0237642
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2024-11-05 16:35:23 +0100

    Change project name

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using my_blazor_project;
+using TphBlazorSpotlightExample;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
