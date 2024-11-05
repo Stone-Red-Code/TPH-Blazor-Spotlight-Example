6e4288104a741cb71f5df49d5af4abfe4f083920
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2024-11-05 23:50:30 +0100

    TPH styling

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using TphBlazorSpotlightExample;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
